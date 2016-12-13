90b9f11053e0d8e92c8c050161ec3443f3ac7790
rinukkusu <rinukkusu@sub-r.de>
2016-12-13 14:04:55 +0100

    fix missing using directive

--- RethinkDbRepository.cs
+++ RethinkDbRepository.cs
@@ -4,6 +4,7 @@ using System.Reflection;
 using RethinkDb.Driver;
 using RethinkDb.Driver.Ast;
 using RethinkDb.Driver.Net;
+using RethinkDBEnhancements.Attributes;
 
 namespace RethinkDBEnhancements
 {
